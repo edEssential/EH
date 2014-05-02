070ae379ff818f4b413d1d0f2859f6787a8bf9d7
madebyeggs <madebyeggs@gmail.com>
2014-05-02 17:07:50 +0100

    css and js made rails friendly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,9 @@
 //= require jquery
 //= require jquery_ujs
-//= require jquery-1.7.2.min
-//= require plugins
-//= require options
-//= require custom
-//= require_tree .
+//= require foundation.min
+//= require index
+//= require jquery.fitvids
+//= require bootstrap
 
 $(document).ready(function () {
 	$('.page').hide();	
@@ -26,3 +25,65 @@ $('.thumb').hover(function(){
 $(function() {
     $( "#tabs" ).tabs();
 });
+
+$(window).load(function() {               $('#status').fadeOut('slow');               $('#preloader').delay(350).fadeOut('slow');
+  });	
+  $(document).foundation();
+  $(document).ready(function () {
+      // Handler for .ready() called.
+      $('#arrow-container').click(function () {
+          $('html, body').animate({
+              scrollTop: $('.index-wrapper').offset().top
+          }, 800);
+      });
+  });
+  $(document).ready(function() {
+  	$(".topLink").click(function() {
+  		$("html, body").animate({
+  			scrollTop: 0	
+  		}, {
+  			duration: 800,
+  			easing: "swing"
+  		});
+  		return false;
+  	});
+
+  });
+  $(document).ready(function() {
+
+  	$("a").each(function () {
+  	    if (this.hostname != document.location.hostname) this.target = "_blank";
+  	});
+
+  });
+  $(document).ready(function() {
+
+      var anchor = document.querySelectorAll('#button-wrapper button');
+ 
+      [].forEach.call(anchor, function(anchor){
+        var open = false;
+        anchor.onclick = function(event){
+          event.preventDefault();
+          if(!open){
+            $('#splash button').addClass('button-fixed');
+            this.classList.add('close');
+            open = true;
+            $('.menu').addClass('openmenu');
+            $('html, body').addClass('fix-body');
+            $('body').bind('touchmove', function(e){e.preventDefault()})
+          }
+          else{
+            $('#splash button').removeClass('button-fixed');
+            this.classList.remove('close');
+            open = false;
+            $('.menu').removeClass('openmenu');
+            $('html, body').removeClass('fix-body');
+            $('body').unbind('touchmove')
+          }
+        }
+      }); 
+    });
+    $(document).ready(function(){
+      // Target your .container, .wrapper, .post, etc.
+      $("#adismalaffair").fitVids();
+    });
